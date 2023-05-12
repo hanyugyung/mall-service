e9d9e6a2905d247d16585e413404c934bfff801e
han60 <hanyugyeong1080@gmail.com>
2023-05-12 14:43:49 +0900

    partner interface 작성

--- mall-service/src/main/kotlin/org/example/interfaces/CommonResponse.kt
+++ mall-service/src/main/kotlin/org/example/interfaces/CommonResponse.kt
@@ -1,4 +1,24 @@
 package org.example.interfaces
 
-class CommonResponse {
+import org.springframework.http.HttpStatus
+
+class CommonResponse<T>(
+    val result: ResultStatus
+    , var data: T? = null
+    , val status: HttpStatus
+) {
+
+    companion object {
+        fun <T> of(result: ResultStatus, data: T, status: HttpStatus): CommonResponse<T> {
+            return CommonResponse(result, data, status)
+        }
+
+        fun of(result: ResultStatus, status: HttpStatus): CommonResponse<*> {
+            return CommonResponse(result, null, status)
+        }
+    }
+
+    enum class ResultStatus {
+        SUCCESS, FAIL
+    }
 }
--- mall-service/src/main/kotlin/org/example/interfaces/partner/PartnerController.kt
+++ mall-service/src/main/kotlin/org/example/interfaces/partner/PartnerController.kt
@@ -1,6 +1,11 @@
 package org.example.interfaces.partner;
 
 import jakarta.validation.Valid
+import lombok.RequiredArgsConstructor
+import org.example.domain.partner.PartnerService
+import org.example.interfaces.CommonResponse
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/partners")
-class PartnerController {
+class PartnerController @Autowired constructor(
+    private val partnerService: PartnerService
+){
 
     @PostMapping("/register")
-    fun registerPartner(@RequestBody @Valid requestDto: PartnerApiDto.RegisterPartner ) {
-
+    fun registerPartner(@RequestBody @Valid requestDto: PartnerApiDto.RegisterPartner ) : CommonResponse<*> {
+        partnerService.registerPartner(requestDto.toDomainDto())
+        return CommonResponse.of(CommonResponse.ResultStatus.SUCCESS, HttpStatus.OK)
     }
 
 }
