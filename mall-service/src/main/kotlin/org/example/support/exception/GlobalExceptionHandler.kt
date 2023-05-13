3a1eded935eb764e83c7beb21cb16bdcef2ab12b
han60 <hanyugyeong1080@gmail.com>
2023-05-13 23:17:11 +0900

    실패 테스트코드 작성 - 사용자 회원가입 이메일 중복 시 실패 테스트코드 추가 - 전역 예외 처리 시 response status 코드 설정

--- mall-service/src/main/kotlin/org/example/support/exception/GlobalExceptionHandler.kt
+++ mall-service/src/main/kotlin/org/example/support/exception/GlobalExceptionHandler.kt
@@ -5,6 +5,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
@@ -12,12 +13,14 @@ class GlobalExceptionHandler {
 
     val logger: Logger = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
 
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(value = [IllegalArgumentException::class])
-    fun exceptionHandler_4xx(exception: RuntimeException): CommonResponse<*> {
+    fun exceptionHandler400(exception: RuntimeException): CommonResponse<*> {
         logger.info("message : {}", exception.message)
         return CommonResponse.successOf(exception, HttpStatus.BAD_REQUEST)
     }
 
+    @ResponseStatus
     @ExceptionHandler(value = [Exception::class])
     fun exceptionHandler(exception: Exception): CommonResponse<*> {
         logger.info("message : {}", exception.message)
